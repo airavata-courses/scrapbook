84864e501cf87f60c7c43f92e15142ecdafb3ec5
Hrishikesh Paul <hrpaul@iu.edu>
2021-02-04 16:25:00 -0500

    chore: added about

--- scrapbook/src/app/modules/home/home.component.ts
+++ scrapbook/src/app/modules/home/home.component.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { faGithub} from '@fortawesome/free-brands-svg-icons'
+import { ViewportScroller } from '@angular/common';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
   faGithub = faGithub;
 
-  constructor() {}
+  constructor(private viewportScroller: ViewportScroller) {}
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+
+  }
+
+  scrollToElement($element): void {
+    console.log($element);
+    $element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
   }
 
 }
