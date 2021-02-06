c0269f8fc36038322b6276674fb62fcb4037d43b
Hrishikesh Paul <hrpaul@iu.edu>
2021-02-06 15:10:18 -0500

    chore: added basic menu

--- scrapbook/src/app/components/sidenav/sidenav.component.ts
+++ scrapbook/src/app/components/sidenav/sidenav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faHome, faShareAlt, faStar, faChartLine } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-sidenav',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidenav.component.scss']
 })
 export class SidenavComponent implements OnInit {
+  
+  sidenavMenu = [
+    {
+      name: 'Home',
+      icon: faHome
+    },
+    {
+      name: 'Shared',
+      icon: faShareAlt
+    },
+    {
+      name: 'Starred',
+      icon: faStar
+    },
+    {
+      name: 'Dashboard',
+      icon: faChartLine
+    }
+  ]
+
+  selectedMenu = 'Home';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onMenuChange(name: string) {
+    this.selectedMenu = this.sidenavMenu.find(i => i.name === name).name;
   }
 
 }
