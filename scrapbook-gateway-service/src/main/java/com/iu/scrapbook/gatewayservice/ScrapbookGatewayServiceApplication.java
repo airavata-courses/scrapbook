90a7d2343db469567736b9a77298fe6fe83ac054
Chirag Shankar Indi <chshan@iu.edu>
2021-02-09 15:00:38 -0500

    chore: basic skeleton code

---
+++ scrapbook-gateway-service/src/main/java/com/iu/scrapbook/gatewayservice/ScrapbookGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.iu.scrapbook.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapbookGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrapbookGatewayServiceApplication.class, args);
+	}
+
+}
