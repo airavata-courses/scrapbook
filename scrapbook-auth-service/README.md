f6e5ce1c2c6d35322e16515a6314a5bcf658f163
Hrishikesh Paul <hrpaul@iu.edu>
2021-02-10 12:26:01 -0500

    chore: updated requirements

---
+++ scrapbook-auth-service/README.md
@@ -0,0 +1,5 @@
+`pip install -r requirements.txt`
+
+`source env/bin/activate`
+
+`python app.py`
--- scrapbook-auth-service/app.py
+++ scrapbook-auth-service/app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from google.oauth2 import id_token
+from google.auth.transport import requests
 app = Flask(__name__)
 
 
@@ -7,4 +9,4 @@ def hello():
     return "Hello World!"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
