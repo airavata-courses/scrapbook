e44d43aff99c46656b897a1bbd5fea365ba1c0ad
Hrishikesh Paul <hrpaul@iu.edu>
2021-02-10 23:30:18 -0500

    chore: improved logic to reset user session

--- scrapbook-session-service/routes/index.js
+++ scrapbook-session-service/routes/index.js
@@ -2,18 +2,29 @@ var express = require("express");
 var router = express.Router();
 var Session = require("../models/session.js");
 
+/**
+ * Route to set the user session. Accepts the UserID and token in the request body
+ */
 router.post("/set", function (req, res, next) {
   const { userID, token } = req.body;
   addUserToSession(res, userID, token);
 });
 
+/**
+ * Route to reset the session timestamp.
+ * First removes the user and then adds them back.
+ */
 router.put('/reset', (req, res, next) => {
   const { userID, token } = req.body;
   Session.remove({userID: userID}, (err, removed) => {
     if (err) {
       res.status(500).send(err);
     } else {
-      addUserToSession(res, userID, token);
+      if(removed.ok === 1) {
+        res.status(401).send('User not in session');
+      } else {
+        addUserToSession(res, userID, token);
+      }
     }
   })
 })
