25d82f56e555758245a2d4ff3e0abc6b2e1a1b63
Chirag Shankar Indi <chshan@iu.edu>
2021-02-12 20:55:31 -0500

    chore: working gateway/need to test withuser service

--- scrapbook-gateway-service/app.py
+++ scrapbook-gateway-service/app.py
@@ -1,26 +1,37 @@
+#for requirements to run the file check requirements.txt
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import urllib
 import sys
 import requests
+from utils import auth_service
 
+#Defines the Flask app
 app = Flask(__name__)
+#Cross Origin Resource Sharing (CORS) is enabled for the app
 CORS(app)
 
+#decorator used to intercept and define route requests
 @app.route('/login', methods=["POST"])
-def authenticate_jwt():
-    #token = request.args.get('idToken', '')
+def retrive_user():
+    '''
+    Route from the login button to retriveing user info from the user service.
+    
+    @params - POST request that Google returns to Client sent to port 8080
+    @return - need to populate depending on user case
+    '''
     try:
-        token = request.args.get('idToken')
-        #print(token)
-        content = requests.get('http://localhost:5000/auth?token='+ token)
-        print(content, file=sys.stderr)
-        #print(content)
+        #is_valid is a True/False values which is returned by the auth_service function in utils.py
+        is_valid = auth_service(request)
         
+        if is_valid == True:
+            #this requests the user service to provide the user data associtaed the with the email ID in the original request
+            response = requests.post('http://localhost:3000/auth', data = {'email': request.form.get('email')})
+            #To debug
+            print(response, file=sys.stderr)
     except:
-        print("not an valid auth request", file=sys.stderr)
-
-    return "cool"
+        print("401", file=sys.stderr)
+    #need to populate returned statement based on the functionality
+    return "", 401
 
 if __name__ == '__main__':
     app.run(host="localhost", port=8080, debug=True)
