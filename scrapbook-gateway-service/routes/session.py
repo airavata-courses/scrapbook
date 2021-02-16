227066f320f6f57c6c0206e695219d8590f1ad54
Chirag Shankar Indi <chshan@iu.edu>
2021-02-16 14:32:09 -0500

    chore: session routes added and tested

--- scrapbook-gateway-service/routes/session.py
+++ scrapbook-gateway-service/routes/session.py
@@ -2,15 +2,12 @@ import requests
 from flask import Blueprint, request, jsonify
 from service_utils import session_service 
 from config import SESSION_SERVICE_URL__DEV
+import sys
 
 logout_user_api = Blueprint('logout_user_api', __name__)
 
 
-<<<<<<< HEAD
 @logout_user_api.route('/logout', methods=["GET"])
-=======
-@logout_user_api.route('/logout', methods=["POST"])
->>>>>>> 426bec911ad2b8992a4b0c56bc24886506741b68
 @session_service.checkUserSession()
 def removeUserSession():
     """
@@ -20,15 +17,12 @@ def removeUserSession():
     @return - http status code
     """
     try:
-<<<<<<< HEAD
         userID = request.values['id']
         response = requests.delete(f'{SESSION_SERVICE_URL__DEV}/remove/{userID}')
-=======
-        sessionId = request.json['id']
-        response = requests.put(f'{SESSION_SERVICE_URL__DEV}/remove/{sessionId}')
->>>>>>> 426bec911ad2b8992a4b0c56bc24886506741b68
         response.raise_for_status()
-        return response.json(), response.status_code
+        print(response, file = sys.stderr)
+        #delete rest call wont have a json response 
+        return "Removed user from session", response.status_code
 
     except requests.exceptions.HTTPError as err:
         return err.response.text, err.response.status_code
