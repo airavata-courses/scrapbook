1ed9bdc5f84264fb79d1e33932af46496b00630a
Chirag Shankar Indi <chshan@iu.edu>
2021-02-27 03:38:19 -0500

    chore:added endpoints

--- scrapbook-classifier-service/app.py
+++ scrapbook-classifier-service/app.py
@@ -2,13 +2,18 @@ from flask import Flask, request, jsonify
 from utils import get_predictions
 from flask_cors import CORS
 import sys
+from config import MONGO_URI
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 CORS(app)
 
 
+app.config["MONGO_URI"] = MONGO_URI
+mongo = PyMongo(app)
+
 @app.route('/image/classify', methods=["POST"])
-def predictImage():
+def predict_image():
     """
     predict classes for image
     
@@ -18,12 +23,36 @@ def predictImage():
 
     try:
         file = request.files['file']
-        detections = get_predictions(file)  
-        print(detections, file=sys.stderr)
-        return "OK", 200
-    except ValueError as e:
-        return jsonify(e), 401
+        image_id = request.form.get('id')
+        detections = get_predictions(file) 
+        class_tags = {"image_id": image_id, "classLabels": detections}
+        mongo.db.classtags.insert_one(class_tags)
+        return "Successfully added", 200
+    except Exception as e:
+        return "Unsuccessful", 500
+
+
+@app.route('/image/class/fetch', methods=["GET"])
+def retrieve_classlabels():
+    """
+    This function fetches the extracted class labels information from the db
+    
+    @params - image id 
+    @return - the class tags
+    """
+
+    try:
+        image_id = request.args.get('id')
+        print(image_id, file=sys.stderr)
+        tags = mongo.db.classtags.find_one_or_404({"image_id":image_id})
+        return jsonify(tags), 200
+    except Exception as e:
+        return "Not Found", 404
+
 
 
 if __name__ == '__main__':
     app.run(port=10001, debug=True)
+
+
+
--- scrapbook-classifier-service/config.py
+++ scrapbook-classifier-service/config.py
@@ -1,5 +1,1 @@
-AUTH_SERIVCE_URL__DEV = 'http://localhost:5000'
-USER_SERVICE_URL__DEV = 'http://localhost:3000'
-SESSION_SERVICE_URL__DEV = 'http://localhost:3500'
-IMAGE_SERVICE_URL__DEV = 'http://localhost:8080'
-G_DRIVE_SERVICE_URL__DEV = 'http://localhost:9090'
+MONGO_URI = "mongodb://localhost:27017/scrapbookClassifierService"
