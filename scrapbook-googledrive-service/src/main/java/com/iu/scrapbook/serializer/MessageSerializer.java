3caa6c9a73eef3c6a65478b2c00e7c44f489ef2c
jyotibhushan <bhushan.jyoti791@gmail.com>
2021-03-14 14:42:54 -0400

    #126 : Kafka chnages (image)

--- scrapbook-googledrive-service/src/main/java/com/iu/scrapbook/serializer/MessageSerializer.java
+++ scrapbook-googledrive-service/src/main/java/com/iu/scrapbook/serializer/MessageSerializer.java
@@ -44,19 +44,4 @@ public class MessageSerializer implements Serializer<Message> {
     public void close() {
 
     }
-
-//    @Override public void configure(Map<String, ?> map, boolean b) {
-//
-//    }
-//
-//    @Override public byte[] serialize(String arg0, Message arg1) {
-//        byte[] retVal = null;
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        try {
-//            retVal = objectMapper.writeValueAsString(arg1).getBytes();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        return retVal;
-//    }
 }
