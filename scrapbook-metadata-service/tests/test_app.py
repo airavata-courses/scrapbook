484d02409f67f6c8caae82ec4604ce5aeb702517
vivekk <vivek.ka93@gmail.com>
2021-03-15 16:44:53 -0400

    cleanup

--- scrapbook-metadata-service/tests/test_app.py
+++ scrapbook-metadata-service/tests/test_app.py
@@ -10,31 +10,14 @@ from app import mongo
 
 @pytest.fixture
 def app():
-    yield flask_app
-
-
-
-
-@pytest.fixture
-def app():
     """Create application for the tests."""
-    # _app = flask_app
     flask_app.logger.setLevel(logging.CRITICAL)
     ctx = flask_app.test_request_context()
     ctx.push()
 
     flask_app.config["TESTING"] = True
     flask_app.testing = True
-
-    # This creates an in-memory sqlite db
-    # See https://martin-thoma.com/sql-connection-strings/
     flask_app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
-
-    # with _app.app_context():
-    #     mongo.create_all()
-    #     author1 = Author(id=1, first_name="foo", last_name="bar")
-    #     mongo.session.add(author1)
-    #     mongo.session.commit()
     yield flask_app
     ctx.pop()
 
