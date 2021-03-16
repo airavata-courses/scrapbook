0b06e4447c22d55a0f605242e65c913adb9435ea
Hrishikesh Paul <hrpaul@iu.edu>
2021-03-16 13:01:37 -0400

    chore: fixed test caes

--- scrapbook-metadata-service/tests/test_app.py
+++ scrapbook-metadata-service/tests/test_app.py
@@ -1,17 +1,8 @@
 import pytest
-from app import app as flask_app
 import json
+from mde_utils import starter
 
-@pytest.fixture
-def app():
-    yield flask_app
-
-@pytest.fixture
-def client(app):
-    return app.test_client()
-
-def test_start(app, client):
-    res = client.get('/')
-    assert res.status_code == 200
+def test_start():
+    res = starter()
     expected = 'Meta Data Extractor Service Started'
-    assert expected == res.get_data(as_text=True)
+    assert expected == res
