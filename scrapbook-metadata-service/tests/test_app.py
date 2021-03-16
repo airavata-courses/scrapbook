1e45c26112bdd8b6e90269bb120486a4d10aba17
Hrishikesh Paul <hrpaul@iu.edu>
2021-03-16 10:10:44 -0400

    chore: cleanup

--- scrapbook-metadata-service/tests/test_app.py
+++ scrapbook-metadata-service/tests/test_app.py
@@ -2,32 +2,6 @@ import pytest
 from app import app as flask_app
 import json
 
-
-# @pytest.fixture
-# def app():
-#     """Create application for the tests."""
-#     flask_app.logger.setLevel(logging.CRITICAL)
-#     ctx = flask_app.test_request_context()
-#     ctx.push()
-
-#     flask_app.config["TESTING"] = True
-#     flask_app.testing = True
-#     # flask_app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
-#     yield flask_app
-#     ctx.pop()
-
-
-
-# @pytest.fixture
-# def client(app):
-#     client = app.test_client()
-#     yield client
-
-
-# def test_app(client):
-#     rv = client.get("/")
-#     assert rv.data == b"Meta Data Service Started"
-
 @pytest.fixture
 def app():
     yield flask_app
