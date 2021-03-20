bb333a95761955b2523d9932a6223925b43132d6
Hrishikesh Paul <hrpaul@iu.edu>
2021-03-20 12:20:08 -0400

    chore: added redis password

--- scrapbook-session-service/config/redis.js
+++ scrapbook-session-service/config/redis.js
@@ -1,9 +1,12 @@
 const redis = require('redis');
+var url = require('url');
 const redisPort = process.env.REDIS_PORT;
 const redisHost = process.env.REDIS_HOST
+const redisPassword = process.env.REDIS_PASSWORD;
 const client = redis.createClient({
   host: redisHost,
-  port: redisPort
+  port: redisPort,
+  password: redisPassword
 });
 
 function init() {
