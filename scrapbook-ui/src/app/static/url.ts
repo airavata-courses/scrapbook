2e55db3abbcabb291fe0b66a742a5b8da4f10891
Hrishikesh Paul <hrpaul@iu.edu>
2021-03-21 16:55:44 -0400

    chore: updated port

--- scrapbook-ui/src/app/static/url.ts
+++ scrapbook-ui/src/app/static/url.ts
@@ -1,5 +1,5 @@
 import { environment } from 'src/environments/environment';
 
-export const GATEWAY_URL = environment.production ? 'http://sb-gateway-service:8081' : 'http://localhost:8081'
+export const GATEWAY_URL = environment.production ? 'http://sb-gateway-service:30001' : 'http://localhost:8081'
 export const GOOGLE_DRIVE_SERVICE_URL = 'http://localhost:9090';
 export const IMAGE_SERVICE_URL = 'http://localhost:8080';
