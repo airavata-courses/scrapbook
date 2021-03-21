d2ea485c110c71c5cbc15773b1edbdc0b77a3b22
Hrishikesh Paul <hrpaul@iu.edu>
2021-03-21 13:55:53 -0400

    chore: changed gateway url

--- scrapbook-ui/src/app/static/url.ts
+++ scrapbook-ui/src/app/static/url.ts
@@ -1,4 +1,4 @@
-export const GATEWAY_URL = 'http://localhost:8081';
+export const GATEWAY_URL = 'http://sb-gateway-service:8081';
 
 export const GOOGLE_DRIVE_SERVICE_URL = 'http://localhost:9090';
 
