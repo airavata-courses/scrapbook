5ad6f6311826d50ea3ca9b9ad2341dae3fb44544
Hrishikesh Paul <hrpaul@iu.edu>
2021-03-22 14:24:47 -0400

    added meta data yaml and ref to clusterIP for all other services

--- start.sh
+++ start.sh
@@ -3,3 +3,6 @@
 kubectl apply -f scrapbook-auth-service/scrapbook-auth-service.yaml
 kubectl apply -f scrapbook-session-service/scrapbook-session-service.yaml
 kubectl apply -f scrapbook-gateway-service/scrapbook-gateway-service.yaml
+kubectl apply -f scrapbook-user-service/scrapbook-user-service.yaml
+
+kubectl apply -f scrapbook-metadata-service/scrapbook-metadata-service.yaml
