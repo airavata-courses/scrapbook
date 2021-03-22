61dfca87ea5748037b59b3b318df2c90066d00d5
Hrishikesh Paul <hrpaul@iu.edu>
2021-03-22 14:45:37 -0400

    chore: config kafka and mde

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+kubectl apply -f scrapbook-kafka/scrapbook-zookeeper.yaml
+kubectl apply -f scrapbook-kafka/scrapbook-broker.yaml
 kubectl apply -f scrapbook-auth-service/scrapbook-auth-service.yaml
 kubectl apply -f scrapbook-session-service/scrapbook-session-service.yaml
 kubectl apply -f scrapbook-gateway-service/scrapbook-gateway-service.yaml
