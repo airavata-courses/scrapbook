3ed2bc8d138bbc6ba2796dd73a6da888bae70be3
Hrishikesh Paul <hrpaul@iu.edu>
2021-03-29 12:42:50 -0400

    chore: updated start script

--- start.sh
+++ start.sh
@@ -7,40 +7,40 @@ kubectl delete deployments --ignore-not-found=true kafka-broker0 sb-auth sb-gate
 kubectl delete services --ignore-not-found=true sb-auth sb-gatetway sb-gdrive sb-image sb-kafka sb-session sb-ui sb-user zoo1
 
 echo '\nDeploy Kafka\n'
-git checkout main-kafka
+git checkout dev-kafka
 kubectl apply -f scrapbook-kafka/scrapbook-zookeeper.yaml
 kubectl apply -f scrapbook-kafka/scrapbook-broker.yaml
 
 echo '\nDeploy Gateway\n'
-git checkout main-gateway
+git checkout dev-gateway
 kubectl apply -f scrapbook-gateway-service/scrapbook-gateway-service.yaml
 
 echo '\nDeploy Auth Service\n'
-git checkout main-auth-service
+git checkout dev-auth-service
 kubectl apply -f scrapbook-auth-service/scrapbook-auth-service.yaml
 
 echo '\nDeploy Session Service\n'
-git checkout main-session-service
+git checkout dev-session-service
 kubectl apply -f scrapbook-session-service/scrapbook-session-service.yaml
 
 echo '\nDeploy User Service\n'
-git checkout main-user-service
+git checkout dev-user-service
 kubectl apply -f scrapbook-user-service/scrapbook-user-service.yaml
 
 echo '\nDeploy Image Service\n' 
-git checkout main-image-service
+git checkout dev-image-service
 kubectl apply -f scrapbook-image-service/scrapbook-image-service.yaml
 
 echo '\nDeploy Google Drive Service\n'
-git checkout main-gdrive-service
+git checkout dev-gdrive-service
 kubectl apply -f scrapbook-googledrive-service/scrapbook-googledrive-service.yaml
 
 echo '\nDeploy MDE Service\n'
-git checkout main-mde-service
+git checkout dev-mde-service
 kubectl apply -f scrapbook-metadata-service/scrapbook-metadata-service.yaml
 
 echo '\nDeploy UI\n'
-git checkout main-UI
+git checkout dev-UI
 kubectl apply -f scrapbook-ui/scrapbook-ui.yaml
 
 
