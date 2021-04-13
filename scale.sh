398302c1d3f8c4e7077b198bba812b616d167f6b
Hrishikesh Paul <hrpaul@iu.edu>
2021-04-13 10:34:21 -0400

    chore: update script

--- scale.sh
+++ scale.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-# Deployments list
-# sb-auth 
-# sb-gateway
-# sb-gdrive 
-# sb-image 
-# sb-metadata
-# sb-session
-# sb-ui
-# sb-user
+if [ $2 == "all" ]; then
+    echo "Scaling $2 to $1 replica(s)"
+    kubectl scale --replicas=$1 deployment sb-auth
+    kubectl scale --replicas=$1 deployment sb-gateway
+    kubectl scale --replicas=$1 deployment sb-gdrive
+    kubectl scale --replicas=$1 deployment sb-image
+    kubectl scale --replicas=$1 deployment sb-metadata
+    kubectl scale --replicas=$1 deployment sb-session
+    kubectl scale --replicas=$1 deployment sb-user
+    kubectl scale --replicas=$1 deployment sb-ui
+else
+    echo "Scaling $2 to $1 replica(s)"
+    kubectl scale --replicas=$1 deployment $2
+fi
 
-kubectl scale --replicas=$1 deployment $2
+
